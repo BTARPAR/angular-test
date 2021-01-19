a8c4dabf21f2864e5ce87a9fd8974ba99a9bb995
Bhargav Tarpara <bhargav_tarpara@yahoo.co.in>
2021-01-18 18:14:24 -0800

    SET-UP: Deployment

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.get(express.static(__dirname + '/dist/angular-test/'))
+app.get('/*', (req, res) => {
+  res.sendFile((path.join(__dirname + '/dist/angular-test/index.html')))
+})
+
+app.listen(PORT, () => {
+  console.log('Your Server is UP')
+})
