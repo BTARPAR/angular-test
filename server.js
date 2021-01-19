13e37af154f3a74c206f49f6f8976dd472a740e0
Bhargav Tarpara <tarpara_bhargav@yahoo.in>
2021-01-18 18:31:53 -0800

    SET-UP: Deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
 
-app.use(express.static(__dirname + '/dist/angular-test/'))
+app.use(express.static(__dirname + '/dist/angular-test'))
 app.get('/*', (req, res) => {
   res.sendFile((path.join(__dirname + '/dist/angular-test/index.html')))
 })
